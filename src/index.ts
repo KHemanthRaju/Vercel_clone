7c66aec1b3ed3611e6c0ee4e03c5f065e07ba3b6
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2024-02-10 18:49:51 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/deploy", (req, res) => {
-  const repoUrl = req.body.repoUrl;
+app.post("/deploy", async (req, res) => {
+  const repoUrl = req.body.repoUrl; //github repo
+  const id = generate();
+  await simpleGit().clone(repoUrl, `output/${id}`);
   console.log(repoUrl);
 
   res.json({});
