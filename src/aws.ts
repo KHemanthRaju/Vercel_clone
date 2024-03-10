1e19f734bdac2d8f5e71be324893bc1d94797964
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2024-03-10 23:30:32 +0530

    Update aws.ts

--- src/aws.ts
+++ src/aws.ts
@@ -1,10 +1,6 @@
 import { S3 } from "aws-sdk";
 import fs from "fs";
 
-const s3 = new S3({
-  accessKeyId: "AKIATVCNZMDNMAH4X4WJ",
-  secretAccessKey: "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",
-});
 
 export const uploadFile = async (fileName: string, localFilePath: string) => {
   console.log("called");
